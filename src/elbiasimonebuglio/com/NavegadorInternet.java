56f7e26331b0ea6beec65c643f07cbe20da0a481
unknown <elbiasimone@hotmail.com>
2023-10-15 22:42:45 -0300

    interface

---
+++ src/elbiasimonebuglio/com/NavegadorInternet.java
@@ -0,0 +1,8 @@
+package elbiasimonebuglio.com;
+
+public interface NavegadorInternet {
+
+    public void exibirPagina(String url);
+    public void adicionarNovaAba(String url);
+    public void atualizarPagina();
+}
